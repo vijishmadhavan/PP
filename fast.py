7cc073d58067edb9de700aaeedac830c5fb390a4
Vijish Madhavan <vijishmadhavan@gmail.com>
2023-03-29 16:16:23 +0530

    Update fast.py

--- fast.py
+++ fast.py
@@ -14,9 +14,8 @@ ENDPOINT_URL = os.environ.get('ENDPOINT_URL')
 HF_TOKEN = os.environ.get('HF_TOKEN')
 
 
-def encode_image(image_path):
-  with open(image_path, "rb") as i:
-    b64 = base64.b64encode(i.read())
+def encode_image(image_bytes):
+  b64 = base64.b64encode(image_bytes.read())
   return b64.decode("utf-8")
 
 
@@ -41,13 +40,11 @@ st.title("Product Photography")
 
 # Get user input
 prompt = st.text_input("Enter a prompt:", "sneakers kept on sea sand")
-image_path = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-if image_path is not None:
-    image_bytes = image_path.name
+image_bytes  = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 # Make prediction and display result
-if image_path is not None:
-    st.image(image_path, caption="Uploaded Image", use_column_width=True)
+if image_bytes is not None:
+    st.image(image_bytes, caption="Uploaded Image", use_column_width=True)
     prediction = predict(prompt, image_path=image_bytes, negative_prompt="worst quality, low quality, oil painting, historic", controlnet_type="canny_edge")
     st.image(prediction, caption="Predicted Image", use_column_width=True)
 else:
